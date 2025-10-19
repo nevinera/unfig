e70681b2477a5a4166516c34d06acb991dcef576
Eric Mueller <nevinera@gmail.com>
2025-10-19 07:57:23 -0400

    Do _not_ use the env_prefix when env is supplied

--- lib/unfig/param_config.rb
+++ lib/unfig/param_config.rb
@@ -56,7 +56,7 @@ module Unfig
 
     def env
       if data.key?(:env)
-        env_prefix + data.fetch(:env, nil)
+        data.fetch(:env, nil)
       else
         env_prefix + name.upcase
       end
